9c4a3add61380d4dfa8b6a0653abe8cf9bb73e0d
Emil Teurnberg <e.teurnberg@gmail.com>
2017-09-03 19:34:47 +0200

    Added handling of page up and down keys.
    They result in moving the cursor to the top and bottom of the page,
    respectively.
    
    Also fixed a typo in the title of editorProcessKeypress.

--- kilo.c
+++ kilo.c
@@ -14,10 +14,12 @@
 #define CTRL_KEY(k) ((k) & 0x1f)
 
 enum editorKey {
-  ARROW_LEFT = 1000,
+ARROW_LEFT = 1000,
   ARROW_RIGHT,
   ARROW_UP,
-  ARROW_DOWN
+  ARROW_DOWN,
+  PAGE_UP,
+  PAGE_DOWN
 };
 
 /*** data ***/
@@ -74,11 +76,21 @@ int editorReadKey() {
     if (read(STDIN_FILENO, &seq[1], 1) != 1) return '\x1b';
 
     if (seq[0] == '[') {
-      switch (seq[1]) {
-        case 'A': return ARROW_UP;
-        case 'B': return ARROW_DOWN;
-        case 'C': return ARROW_RIGHT;
-        case 'D': return ARROW_LEFT;
+      if (seq[1] >= '0' && seq[1] <= '9') {
+        if (read(STDIN_FILENO, &seq[2], 1) != 1) return '\x1b';
+        if (seq[2] == '~') {
+          switch (seq[1]) {
+            case '5': return PAGE_UP;
+            case '6': return PAGE_DOWN;
+          }
+        }
+      } else {
+        switch (seq[1]) {
+          case 'A': return ARROW_UP;
+          case 'B': return ARROW_DOWN;
+          case 'C': return ARROW_RIGHT;
+          case 'D': return ARROW_LEFT;
+        }
       }
     }
 
@@ -215,7 +227,7 @@ void editorMoveCursor(int key) {
   }
 }
 
-void editorProcessKeyPress() {
+void editorProcessKeypress() {
   int c = editorReadKey();
 
   switch(c) {
@@ -224,6 +236,16 @@ void editorProcessKeyPress() {
       write(STDOUT_FILENO, "\x1b[H", 3);
       exit(0);
       break;
+
+    case PAGE_UP:
+    case PAGE_DOWN:
+      {
+        int times = E.screenrows;
+        while (times--)
+          editorMoveCursor(c == PAGE_UP ? ARROW_UP : ARROW_DOWN);
+      }
+      break;
+
     case ARROW_UP:
     case ARROW_DOWN:
     case ARROW_LEFT:
@@ -248,7 +270,7 @@ int main() {
 
   while (1) {
     editorRefreshScreen();
-    editorProcessKeyPress();
+    editorProcessKeypress();
   }
 
   return 0;
